c0fbf7022fb137526f0aca18b2e70aa1bab68b94
Ofer Guez <guez.ofer@gmail.com>
2024-11-12 19:59:23 +0000

    1

--- src/services/SoundService.ts
+++ src/services/SoundService.ts
@@ -32,7 +32,7 @@ export class SoundService {
     const randomFile = availableFiles[Math.floor(Math.random() * availableFiles.length)];
     this.playedFiles.push(randomFile);
 
-    const soundPath = `public/Arithmetic-/VictorySounds/${randomFile}`; //debugger;
+    const soundPath = `public/VictorySounds/${randomFile}`; //debugger;
     // Fetch and decode the audio
     const response = await fetch(soundPath);
     if (!response.ok) {
